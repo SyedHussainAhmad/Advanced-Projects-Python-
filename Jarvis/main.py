64a340fa1e308ad62c7b33a9af5cf2d746e04bec
Hussain <hsyed9877@gmail.com>
2022-11-22 23:55:35 +0500

    Added Project

--- Jarvis/main.py
+++ Jarvis/main.py
@@ -1,13 +1,10 @@
-# Import Modules:
-import pyttsx3 
-import speech_recognition as sr 
-import wikipedia # --> To interact with your wIkipedia
+'''Import Modules:'''
+from datetime import datetime # --> For date & time.
 import webbrowser # --> To search across web.
-import os # --> To interact with your operating system
-import smtplib 
-from youtubesearchpython import Search # --> To search something on youtube.
-import datetime # --> For date & time.
-from AppOpener import run
+import wikipedia # --> To interact with your wIkipedia
+from AppOpener import run # --> To run the app.
+import speech_recognition as sr # --> For speech recognition
+import pyttsx3 # --> Text to speech.
 
 
 # Voice Initialization:
@@ -16,92 +13,87 @@ voice = engine.getProperty('voices')
 engine.setProperty('voice', voice[0].id)
 
 # voice[0].id --> Male Voice.
-# voice[1].id --> Feale Voice.
+# voice[1].id --> Female Voice.
 
 # Functions:
 
 def speak(audio):
-    # This Function speaks the input value.
-
+    '''This Function speaks the input value.'''
     engine.say(audio)
     engine.runAndWait()
 
 def greeting():
-    # This Function greets the user.
-
-    hour = int(datetime.datetime.now().hour)
+    '''This Function greets the user.'''
+    hour = int(datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning Hussain")
 
     elif hour>=12 and hour<18:
-        speak("Good Afternoon Hussain")   
+        speak("Good Afternoon Hussain")
 
     else:
         speak("Good Evening Hussain")
 
-    speak("I am Jarvis Sir. How may I help you")       
-
-
+    speak("I am Jarvis Sir. How may I help you")
 
 def listen():
-    # It takes microphone input from the user and returns string output
+    ''' It takes microphone input from the user and returns string output'''
+    recognizer = sr.Recognizer()
 
-    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
+        recognizer.pause_threshold = 1
+        audio = recognizer.listen(source)
 
     try:
-        print("Recognizing your command...")    
-        query = r.recognize_google(audio)
-        print(f"You said: {query}\n")
+        print("Recognizing your command...")
+        voice_command = recognizer.recognize_google(audio)
+        print(f"You said: {voice_command}\n")
 
-    except Exception as e:
-        print("Please say again...")  
+    except LookupError:
+        print("Please say again...")
         return "None"
 
-    return query
+    return voice_command
 
-def search(query):
-    # This function search user commands on internet.
-    speak(f'Searching')
-    query = query.replace('search', '')
-    webbrowser.open(query)
+def search(input_query):
+    '''This function search user commands on internet.'''
+    speak('Searching')
+    input_query = input_query.replace('search', '')
+    webbrowser.open(input_query)
 
-def website(query):
-    # This function opens the website user wants.
-    speak(f'Openning')
-    query = query.replace('open', '')
-    query = query.replace(' ', '')
-    try:
-        run(query)
-    except:
-        searchItem = f'{query}.com'
-        webbrowser.open(searchItem)
+def website(input_query):
+    '''This function opens the apps user wants.'''
+    speak('Openning')
+    input_query = input_query.replace('open', '')
+    run(input_query)
 
-
-def wiki(query):
-    # This function fetch the information from wikipedia.
+def wiki(input_query):
+    '''This function fetch the information from wikipedia.'''
     speak('Searching Wikipedia...')
     print('Searching Wikipedia...')
-    query = query.replace('wikipedia', '')
-    results = wikipedia.summary(query, sentences = 3)
+    input_query = input_query.replace('wikipedia', '')
+    results = wikipedia.summary(input_query, sentences = 3)
     speak('According to wikipedia..')
     print(results)
     speak(results)
 
+def date():
+    '''This function tells today's date.'''
+    today_date = datetime.now().strftime("%B %d, %Y")
+    print(f"today's date is {today_date}")
+    speak(f"today's date is {today_date}")
 
-def sendMail():
-    pass
-
-def dateTime():
-    pass
+def time():
+    '''This function tells current time.'''
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"Sir, the time is {current_time}")
+    speak(f"Sir, the time is {current_time}")
 
 # Main Program
 if __name__ == "__main__":
 
-    # greeting()
+    greeting()
     while True:
 
         query = listen().lower() # --> Take command and convert it into lower case string.
@@ -120,8 +112,15 @@ if __name__ == "__main__":
         elif 'wikipedia' in query:
             wiki(query)
 
+        # Current Time:
+        elif 'time' in query:
+            time()
+
+        # Todays's Date:
+        elif 'date' in query:
+            date()
+
         elif 'exit' or 'quit' in query:
             speak("Exiting... Thankyou for using my services.")
             print("Exiting... Thankyou for using my services.")
             exit()
-        
